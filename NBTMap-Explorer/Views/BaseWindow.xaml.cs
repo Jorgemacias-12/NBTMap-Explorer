c02847d55eec8ea8def4c14cdb4393802a9b7dd0
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-07-01 15:22:09 -0600

    add: BaseWindow (view)
    Add both files .xaml and .xaml.cs

---
+++ NBTMap-Explorer/Views/BaseWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace NBTMap_Explorer.Views
+{
+    public partial class BaseWindow : Window
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint dwFlags);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT { public int x; public int y; }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO { public POINT ptReserved; public POINT ptMaxSize; public POINT ptMaxPosition; public POINT ptMinTrackSize; public POINT ptMaxTrackSize; }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct MONITORINFO { public int cbSize; public RECT rcMonitor; public RECT rcWork; public uint dwFlags; }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT { public int left; public int top; public int right; public int bottom; }
+
+        private const int WM_SYSCOMMAND = 0x0112;
+        private const int SC_CLOSE = 0xF060;
+        private const int SC_MINIMIZE = 0xF020;
+        private const int SC_MAXIMIZE = 0xF030;
+        private const int SC_RESTORE = 0xF120;
+        private const int WM_GETMINMAXINFO = 0x0024;
+        private const int MONITOR_DEFAULTTONEAREST = 2;
+        private const int WM_NCHITTEST = 0x0084;
+        private const int HTMAXBUTTON = 9;
+
+        private Button? _lastHoveredButton = null;
+
+        public string MaxRestoreIcon => WindowState == WindowState.Maximized ? "" : "";
+
+        public BaseWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var helper = new WindowInteropHelper(this);
+            var source = HwndSource.FromHwnd(helper.Handle);
+            source.AddHook(WndProc);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_GETMINMAXINFO)
+            {
+                WmGetMinMaxInfo(hwnd, lParam);
+                handled = true;
+                return IntPtr.Zero;
+            }
+
+            handled = false;
+            return IntPtr.Zero;
+        }
+
+        private void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam)
+        {
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+            IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+            if (monitor != IntPtr.Zero)
+            {
+                MONITORINFO monitorInfo = new MONITORINFO { cbSize = Marshal.SizeOf(typeof(MONITORINFO)) };
+                GetMonitorInfo(monitor, ref monitorInfo);
+                RECT rcWorkArea = monitorInfo.rcWork;
+                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left);
+                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top);
+                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
+                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
+            }
+            Marshal.StructureToPtr(mmi, lParam, true);
+        }
+
+        private void Min_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage(new WindowInteropHelper(this).Handle, WM_SYSCOMMAND, (IntPtr)SC_MINIMIZE, IntPtr.Zero);
+        }
+
+        private void Max_Click(object sender, RoutedEventArgs e)
+        {
+            var command = WindowState == WindowState.Maximized ? SC_RESTORE : SC_MAXIMIZE;
+            SendMessage(new WindowInteropHelper(this).Handle, WM_SYSCOMMAND, (IntPtr)command, IntPtr.Zero);
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage(new WindowInteropHelper(this).Handle, WM_SYSCOMMAND, (IntPtr)SC_CLOSE, IntPtr.Zero);
+        }
+    }
+}
