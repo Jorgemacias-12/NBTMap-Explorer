1026ae1d082cc1a0d48bf67e2930550f7d7a3800
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-07-01 12:38:20 -0600

    add: BaseWindowViewModel.cs
    This is the implementation of a view model for the base window itself,
    for now it only has one use.
    
    Check for the window state and change an string (that is used as an
    icon).

---
+++ NBTMap-Explorer/ViewModels/BaseWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using NBTMap_Explorer.ViewModels.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace NBTMap_Explorer.ViewModels
+{
+    public class BaseWindowViewModel : ViewModelBase
+    {
+        private WindowState _windowState;
+        public WindowState WindowState
+        {
+            get => _windowState;
+            set
+            {
+                if (_windowState != value)
+                {
+                    _windowState = value;
+                    OnPropertyChanged(nameof(WindowState));
+                    OnPropertyChanged(nameof(MaxRestoreIcon));
+                }
+            }
+        }
+
+        public string MaxRestoreIcon => WindowState == WindowState.Maximized ? "\uE923" : "\uE922";
+
+
+        public ICommand MinimizeCommand { get; }
+        public ICommand MaximizeCommand { get; }
+        public ICommand CloseCommand { get; }
+
+        public Action? RequestClose { get; set; }
+        public Action? RequestMinimize { get; set; }
+        public Action? RequestMaximize { get; set; }
+
+        public BaseWindowViewModel()
+        {
+            MinimizeCommand = new RelayCommand(_ => RequestMinimize?.Invoke());
+            MaximizeCommand = new RelayCommand(_ => RequestMaximize?.Invoke());
+            CloseCommand = new RelayCommand(_ => RequestClose?.Invoke());
+        }
+    }
+}
