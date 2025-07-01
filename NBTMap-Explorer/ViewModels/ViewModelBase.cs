ffa07a1fba1970d0ca1f9ae465c4dfc3f17ef2d4
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-07-01 12:34:43 -0600

    add: ViewModelBase
    As his name says this file is abstract and it must be used as a
    ViewModel base in any other ViewModel for this project, also uses
    INotifyPropertyChanged Interface with OnPropertyChanged, in the future
    will contain more props or actions even events as the project grows

---
+++ NBTMap-Explorer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace NBTMap_Explorer.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
