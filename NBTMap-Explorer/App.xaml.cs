d763a8f873561bf96026e7ab2a91f6dbbc5fc876
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-07-05 19:08:50 -0600

    update: App.xaml.cs:
    Prepare file to launch splash screen instead of BaseWindow.

--- NBTMap-Explorer/App.xaml.cs
+++ NBTMap-Explorer/App.xaml.cs
@@ -6,26 +6,8 @@ namespace NBTMap_Explorer
 {
     public partial class App : Application
     {
-        public IServiceProvider ServiceProvider { get; private set; }
-
         protected override void OnStartup(StartupEventArgs e)
         {
-            var services = new ServiceCollection();
-
-            ConfigureServices(services);
-
-            ServiceProvider = services.BuildServiceProvider();
-
-            var window = new BaseWindow();
-
-            window.Show();
-        }
-
-        private void ConfigureServices(ServiceCollection services)
-        {
-            // Register your services here
-            // Example: services.Add
-
         }
     }
 }
