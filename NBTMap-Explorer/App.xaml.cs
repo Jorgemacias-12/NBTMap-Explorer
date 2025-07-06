50fcc6a975b3480a0f8d80e80222dfe2bb1daaab
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-07-05 19:43:08 -0600

    add: SplashScreen implementation
    This commit refers to task #21

--- NBTMap-Explorer/App.xaml.cs
+++ NBTMap-Explorer/App.xaml.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using NBTMap_Explorer.Views;
+﻿using NBTMap_Explorer.Views;
 using System.Windows;
+using SplashScreen = NBTMap_Explorer.Views.SplashScreen;
 
 namespace NBTMap_Explorer
 {
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
+        protected override async void OnStartup(StartupEventArgs e)
         {
+            base.OnStartup(e);
+
+
+            SplashScreen splashScreen = new SplashScreen();
+            
+            splashScreen.Show();
         }
     }
 }
---
+++ NBTMap-Explorer/Views/SplashScreen.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NBTMap_Explorer.Views
+{
+    /// <summary>
+    /// Lógica de interacción para SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
