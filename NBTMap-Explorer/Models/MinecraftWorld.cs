5ac1e5cae932847c3bd136dbbaed284adceef67d
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-07-10 13:13:49 -0600

    update: MinecraftWorld model #23
    Remove Image type use Path instead and load it in runtime.
    
    And clean non used imports

--- NBTMap-Explorer/Models/MinecraftWorld.cs
+++ NBTMap-Explorer/Models/MinecraftWorld.cs
@@ -1,13 +1,11 @@
-﻿using System.Windows.Controls;
-
-namespace NBTMap_Explorer.Models
+﻿namespace NBTMap_Explorer.Models
 {
     public class MinecraftWorld
     {
         public string Name { get; init; }
         public string Path { get; init; }
-        public string Version { get; init; }
-        public Image? SplashIcon { get; init; }
+        public string Version { get; init; } 
+        public string SplashIconPath { get; init; }
         public bool HasMaps { get; init; }
         public List<MinecraftMap> Maps { get; init; } = [];
     }
