3a6f6644ca7fdf66d50f14e981b89883bb0a8062
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-07-10 13:39:25 -0600

    update: JavaWorldLoader (service) #23
    Implement All and individual methods.

--- NBTMap-Explorer/Services/JavaWorldLoader.cs
+++ NBTMap-Explorer/Services/JavaWorldLoader.cs
@@ -1,18 +1,121 @@
-﻿using NBTMap_Explorer.Models;
+﻿using fNbt;
+using Serilog;
+using NBTMap_Explorer.Helpers;
+using NBTMap_Explorer.Models;
 using NBTMap_Explorer.Services.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace NBTMap_Explorer.Services
 {
     public class JavaWorldLoader : IWorldLoader
     {
-        public Task<MinecraftMap[]> LoadMapsAsync(string worldPaths)
+        private static readonly ILogger _log = Log.ForContext<JavaWorldLoader>();
+
+        public Task<List<MinecraftWorld>> LoadAllWorldsAsync()
         {
-            throw new NotImplementedException();
+            return Task.Run(() =>
+            {
+                if (!Directory.Exists(Minecraft.JAVA_SAVES_PATH))
+                {
+                    return new List<MinecraftWorld>();
+                }
+
+                string[] worldDirectories = Directory.GetDirectories(Minecraft.JAVA_SAVES_PATH);
+
+                var worlds = new List<MinecraftWorld>();
+
+                const string splashIconFileName = "icon.png";
+                const string levelDatFileName = "level.dat";
+
+                foreach (var dir in worldDirectories)
+                {
+                    string levelDatPath = Path.Combine(dir, levelDatFileName);
+                    string splashIconPath = Path.Combine(dir, splashIconFileName);
+                    bool IsValidWorld = File.Exists(levelDatPath);
+
+                    if (!IsValidWorld)
+                    {
+                        continue;
+                    }
+
+                    try
+                    {
+                        var worldData = new NbtFile();
+
+                        worldData.LoadFromFile(levelDatPath);
+
+                        string worldVersion = worldData.RootTag?
+                                                       .Get<NbtCompound>("Data")?
+                                                       .Get<NbtCompound>("Version")?
+                                                       .Get<NbtString>("Name")?.Value ?? "Unknown";
+
+                        var world = new MinecraftWorld
+                        {
+                            Name = Path.GetFileName(dir),
+                            Path = dir,
+                            Version = worldVersion,
+                            SplashIconPath = File.Exists(splashIconPath) ? splashIconPath : string.Empty,
+                        };
+
+                        worlds.Add(world);
+                    }
+                    catch (Exception e)
+                    {
+                        _log.Error(e, "Error reading world data from {LevelDatPath} {e}", levelDatPath, e);
+                        continue;
+                    }
+                }
+
+                return worlds;
+            });
+        }
+
+        public Task<MinecraftWorld?> LoadWorldAsync(string worldPath)
+        {
+            if (String.IsNullOrEmpty(worldPath) || !Directory.Exists(worldPath))
+            {
+                return Task.FromResult<MinecraftWorld?>(null);
+            }
+
+            return Task.Run(() =>
+            {
+                const string splashIconFileName = "icon.png";
+                const string levelDatFileName = "level.dat";
+
+                string levelDatPath = Path.Combine(worldPath, levelDatFileName);
+                string splashIconPath = Path.Combine(worldPath, splashIconFileName);
+                bool IsValidWorld = File.Exists(levelDatPath);
+
+                if (!IsValidWorld)
+                {
+                    return null;
+                }
+                
+                try
+                {
+                    var worldData = new NbtFile();
+                    
+                    worldData.LoadFromFile(levelDatPath);
+                    
+                    string worldVersion = worldData.RootTag?
+                                                       .Get<NbtCompound>("Data")?
+                                                       .Get<NbtCompound>("Version")?
+                                                       .Get<NbtString>("Name")?.Value ?? "Unknown";
+                    var world = new MinecraftWorld
+                    {
+                        Name = Path.GetFileName(worldPath),
+                        Path = worldPath,
+                        Version = worldVersion,
+                        SplashIconPath = File.Exists(splashIconPath) ? splashIconPath : string.Empty,
+                    };
+                    
+                    return world;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            });
         }
     }
 }
